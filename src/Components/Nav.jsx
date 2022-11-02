c46df61439c8ed06423767b90509dec2db7c0575
MShafiMS <shahedali734@gmail.com>
2022-11-02 22:15:45 +0600

    updated

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Nav = () => {
     return (
         <div>
-            <h1>Navbar</h1>
+            <h1 className='text-4xl'>Navbar</h1>
         </div>
     );
 };
