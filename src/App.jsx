92649e81c8302c3ff02018e8186b3b97c3f18c5a
Shakhawathh <shakhawathh55@gmail.com>
2022-11-03 12:40:57 +0600

    frature add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Nav from "./Components/Nav";
+import Home from "./Components/page/Home";
 
 function App() {
   return (
     <>
-      <Nav />
+     
+      <Home/>
     </>
   );
 }
