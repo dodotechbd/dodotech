33f96a1e522c20c2dd2f8028d605cee98a303ff2
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-11-06 00:24:28 +0600

    added setup all

--- src/Pages/template/Templates.jsx
+++ src/Pages/template/Templates.jsx
@@ -20,12 +20,12 @@ const Templates = () => {
         <div>
             <div className='grid lg:grid-cols-2 sm:grid-cols-1 gap-10 my-10'>
                 <div className='text-center'>
-                    <h1 className='text-4xl font-bold'>Our Template Are Just</h1>
-                    <h1 className='text-3xl'>Ready to Use</h1>
+                    <h1 className='text-5xl font-bold'>Our Template Are Just</h1>
+                    <h1 className='text-5xl font-bold'>Ready to Use</h1>
                 </div>
-                <div className='text-center'>
-                    <p className='text-4xl'>You will love all of the features in our template.</p>
-                    <p className='text-3xl'>100% guaranteed satisfaction.</p>
+                <div className='text-center text-2xl p-5'>
+                    <p className=''>You will love all of the features in our template.</p>
+                    <p className=''>100% guaranteed satisfaction.</p>
                 </div>
             </div>
 
